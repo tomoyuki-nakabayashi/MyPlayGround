19f8e10cd2a4628917239a20c1521da7051a900b
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-06-10 20:02:16 +0900

    checkpoint.

--- MakeOsMyself/day03/haribote.asm
+++ MakeOsMyself/day03/haribote.asm
@@ -1,10 +1,29 @@
 ; haribote-os
 ; TAB=4
+
+; boot info
+CYLS    EQU     0x0ff0      ; set by boot sector
+LEDS    EQU     0x0ff1
+VMODE   EQU     0x0ff2      ; color info
+SCRNX   EQU     0x0ff4      ; resolution x
+SCRNY   EQU     0x0ff6      ; resolution y
+VRAM    EQU     0x0ff8      ; start address of graphic buffer
+
         ORG     0xc200      ; 0xc200 <- 0x8000 + 0x4200
 
         MOV     AL, 0x13    ; VGA graphics 320x200x8bit
         MOV     AH, 0x00
         INT     0x10
+
+        MOV     BYTE [VMODE], 8  ; save video mode
+        MOV     WORD [SCRNX], 320
+        MOV     WORD [SCRNY], 200
+        MOV     DWORD [VRAM], 0x000a0000
+
+; get LED state on keyboard from BIOS
+        MOV     AH, 0x02
+        INT     0x16        ; keyboard BIOS
+        MOV     [LEDS], AL
 
 fin:
         HLT
