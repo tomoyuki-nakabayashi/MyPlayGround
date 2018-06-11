ed2fa5df5460562eb78d933ab110b3baee5d79e1
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-06-11 13:11:20 +0900

    snippets: Rust By Example custom_types/linked_list. #2

--- RustByExample/03.CustomTypes/linked_list/src/main.rs
+++ RustByExample/03.CustomTypes/linked_list/src/main.rs
@@ -23,13 +23,43 @@ impl List {
 
     // Return the length of the list
     fn len(&self) -> u32 {
+        // `self` has to be matched, because the behavior of the method depends on the variant of `self`
+        // `self` has type `&List`, and `*self` has type `List`, matching on a
+        // concrete type `T` is preferred over a match on a reference `&T`
         match *self {
+            // Can't take ownership of the tail, because `self` is borrowed;
+            // instead take a reference to the tail.
             Cons(_, ref tail) => 1 + tail.len(),
+            // Base Case: An empty list has zero length
             Nil => 0
+        }
+    }
+
+    // Return representation of the list as a (heap allocated) string
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                // `format!` is similart to print!, but returns a heap
+                // allocated string instead of printing to the console
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    // Create an empty linked list
+    let mut list = List::new();
+
+    // Prepend some elements
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    // Show the final state of the list
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
 }
