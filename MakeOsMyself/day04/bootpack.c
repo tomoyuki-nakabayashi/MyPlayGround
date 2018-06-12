789e5e3d4171586ebd358ef45c864cc710afc101
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-06-12 21:21:01 +0900

    snippets: Make OS Myself day4 pp.93, Use I/O port. #6

--- MakeOsMyself/day04/bootpack.c
+++ MakeOsMyself/day04/bootpack.c
@@ -1,10 +1,54 @@
 extern void io_hlt(void);
-extern void write_mem8(int addr, int data);
+extern void io_cli(void);
+extern void io_out8(int port, int data);
+extern int io_load_eflags(void);
+extern void io_store_eflags(int eflags);
+
+void set_palette(int start, int end, unsigned char *rgb) {
+  int i, eflags;
+  eflags = io_load_eflags();  // save interrupt enable flag
+  io_cli();  // disable interrupt
+  io_out8(0x03c8, start);
+  for (i = start; i <= end; i++) {
+    io_out8(0x03c9, rgb[0] / 4);
+    io_out8(0x03c9, rgb[1] / 4);
+    io_out8(0x03c9, rgb[2] / 4);
+    rgb += 3;
+  }
+  io_store_eflags(eflags);
+  return;
+}
+
+void init_palette(void) {
+  static unsigned char table_rgb[16*3] = {
+    0x00, 0x00, 0x00,  // black
+    0xff, 0x00, 0x00,  // light red
+    0x00, 0xff, 0x00,  // light green
+    0xff, 0xff, 0x00,  // light yellow
+    0x00, 0x00, 0xff,  // light blue
+    0xff, 0x00, 0xff,  // light purple
+    0x00, 0xff, 0xff,  // turquoise
+    0xff, 0xff, 0xff,  // white
+    0xc6, 0xc6, 0xc6,  // light grey
+    0x84, 0x00, 0x00,  // dark red
+    0x00, 0x84, 0x00,  // dark green
+    0x84, 0x84, 0x00,  // dark yellow
+    0x00, 0x00, 0x84,  // dark blue
+    0x84, 0x00, 0x84,  // dark purple
+    0x00, 0x84, 0x84,  // dark turquoise
+    0x84, 0x84, 0x84,  // dark grey
+  };
+  set_palette(0, 15, table_rgb);
+  return;
+}
 
 void HariMain(void) {
+  init_palette();
+
   int i;
-  for (i = 0xa0000; i < 0xaffff; i++) {
-    write_mem8(i, 15);
+  char *p = (char*)0xa0000;
+  for (i = 0; i < 0xffff; i++) {
+    p[i] = i & 0x0f;
   }
   for(;;) {
     io_hlt();
--- MakeOsMyself/day04/nasmfunc.asm
+++ MakeOsMyself/day04/nasmfunc.asm
@@ -2,15 +2,70 @@
 ; TAB=4
 
 section .text
-    GLOBAL  io_hlt
-    GLOBAL  write_mem8
+    GLOBAL  io_hlt, io_cli, io_sti, io_stihlt
+    GLOBAL  io_in8, io_in16, io_in32
+    GLOBAL  io_out8, io_out16, io_out32
+    GLOBAL  io_load_eflags, io_store_eflags
 
 io_hlt:     ; void io_hlt(void)
     HLT
     RET
 
-write_mem8: ; void write_mem8(int addr, int data);
-    MOV     ECX, [ESP+4]    ; addr => [ESP+4]
-    MOV     AL, [ESP+8]     ; data => [ESP+8]
-    MOV     [ECX], AL
+io_cli:     ; void io_cli(void)
+    CLI
+    RET
+
+io_sti:     ; void io_sti(void)
+    STI
+    RET
+
+io_stihlt:  ; void io_stihlt(void)
+    STI
+    HLT
+    RET
+
+io_in8:     ; int io_in8(int port)
+    MOV     EDX, [ESP+4]
+    MOV     EAX, 0
+    IN      AL, DX
+    RET
+
+io_in16:    ; int io_in16(int port)
+    MOV     EDX, [ESP+4]
+    MOV     EAX, 0
+    IN      AX, DX
+    RET
+
+io_in32:    ; int io_in32(int port)
+    MOV     EDX, [ESP+4]
+    IN      EAX, DX
+    RET
+
+io_out8     ; void io_out8(int port, int data)
+    MOV     EDX, [ESP+4]
+    MOV     EAX, [ESP+8]
+    OUT     DX, AL
+    RET
+
+io_out16    ; void io_out16(int port, int data)
+    MOV     EDX, [ESP+4]
+    MOV     EAX, [ESP+8]
+    OUT     DX, AX
+    RET
+
+io_out32    ; void io_out32(int port, int data)
+    MOV     EDX, [ESP+4]
+    MOV     EAX, [ESP+8]
+    OUT     DX, EAX
+    RET
+
+io_load_eflags: ; int io_load_eflags(void)
+    PUSHFD      ; push eflags
+    POP     EAX
+    RET
+
+io_store_eflags:
+    MOV     EAX, [ESP+4]
+    PUSH    EAX
+    POPFD       ; pop eflags
     RET
