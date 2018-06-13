faadb7e7823642556d72c3fb28fcd6e57acf8763
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-06-13 13:08:35 +0900

    snippets: Rust By Example types/inference. #4

---
+++ RustByExample/05.Types/inference/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Because of the annotation, the compiler knows that `elem` has type u8.
+    let elem = 5u8;
+
+    // Create an empty vector (a growable array).
+    let mut vec = Vec::new();
+    // At this point the compiler doesn't know the exact type of `vec`,
+    // it just knows that it's a vector of something (`Vec<_>`).
+
+    // Insert `elem` in the vector.
+    vec.push(elem);
+    println!("{:?}", vec);
+}
