a770fe197191e279484e89f2fab3bc080d9e1de1
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-06-18 20:44:32 +0900

    snippets: Make OS Myself day5 finish. #6

--- MakeOsMyself/day05/bootpack.c
+++ MakeOsMyself/day05/bootpack.c
@@ -20,7 +20,61 @@ extern void io_cli(void);
 extern void io_out8(int port, int data);
 extern int io_load_eflags(void);
 extern void io_store_eflags(int eflags);
+extern void load_gdtr(int limit, int addr);
+extern void load_idtr(int limit, int addr);
 extern void sprintf (char *str, char *fmt, ...);
+
+struct SEGMENT_DESCRIPTOR {
+  short limit_low, base_low;
+  char base_mid, access_right;
+  char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+  short offset_low, selector;
+  char dw_count, access_right;
+  short offset_high;
+};
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar) {
+  if (limit > 0xfffff) {
+    ar |= 0x8000;  // G_bit = 1
+    limit /= 0x1000;
+  }
+  sd->limit_low = limit & 0xffff;
+  sd->base_low = base & 0xffff;
+  sd->base_mid = (base >> 16) & 0xff;
+  sd->access_right = ar & 0xff;
+  sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+  sd->base_high = (base >> 24) & 0xff;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar) {
+  gd->offset_low = offset & 0xffff;
+  gd->selector = selector;
+  gd->dw_count = (ar >> 8) && 0xff;
+  gd->access_right = ar & 0xff;
+  gd->offset_high = (offset >> 16) & 0xffff;
+}
+
+void init_gdtidt(void) {
+  struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR*) 0x00270000;
+  struct GATE_DESCRIPTOR *idt = (struct GATE_DESCRIPTOR*) 0x0026f800;
+
+  // initialize GDT (zero filled)
+  for (int i = 0; i < 8192; i++) {
+    set_segmdesc(gdt + i, 0, 0, 0);
+  }
+  set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+  set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+  load_gdtr(0xffff, 0x00270000);
+
+  // initalize IDT
+  for (int i = 0; i < 256; i++) {
+    set_gatedesc(idt + i, 0, 0, 0);
+  }
+  load_idtr(0x7ff, 0x0026f800);
+}
 
 struct BOOTINFO {
   char cyls, leds, vmode, reserve;
