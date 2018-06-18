10f5b5690b102effba8b8feec5ffb8a8861e42f1
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-06-18 13:07:13 +0900

    snippets: Rust By Example flow_control/for. #4

---
+++ RustByExample/08.FlowControl/for_loops/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // `n` will take the values: 1, 2, ..., 100 in each iteration
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+}
