0d8fbdee56f1c3d7bc20edb56658707b247fbc6b
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-06-25 13:19:34 +0900

    snippets: Rust By Example fn/closures/std_examples. #4

---
+++ RustByExample/09.Functions/std_example/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    // `iter()` for vecs yields `&i32`. Destructure to `i32`.
+    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+    // `into_iter()` for vecs yields `i32`. No destructuring required.
+    println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+
+
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+    // `iter()` for vecs yields `&i32`.
+    let mut iter = vec1.iter();
+    // `into_iter()` for vecs yields `i32`
+    let mut into_iter = vec2.into_iter();
+
+    // A reference to what is yielded is `&&i32`. Destructure to `i32`.
+    println!("Find 2 in vec1: {:?}", iter.find(|&&x| x == 2));
+    // A reference to what is yielded is `&i32`. Destructure to `i32`.
+    println!("Find 2 in vec2: {:?}", into_iter.find(|&x| x == 2));
+}
