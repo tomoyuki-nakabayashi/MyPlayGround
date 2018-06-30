98ecc2f64c2469820db4bc8a880aac59e3cd8b45
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-01 07:59:31 +0900

    add: Done implementing A Freestanding Rust Binary. #5

--- BlogOS/blog_os/src/main.rs
+++ BlogOS/blog_os/src/main.rs
@@ -1,11 +1,13 @@
 #![feature(lang_items)]
-#![no_std]
-#![no_main]
+#![no_std]  // Don't link the Rust standard library
+#![no_main]  // Disable all Rust-level entry points
 
 #[lang = "panic_fmt"]
 fn panic_fmt() -> ! { loop {} }
 
-#[no_mangle]
+#[no_mangle]  // Don't mangle the name of the function
 pub extern "C" fn _start() -> ! {
+    // This function is the entry point, since the linker looks for a function
+    // named `_start` by default.
     loop {}
 }
