63654e80ccaaf00e63fa3fd19af9f98558eecafd
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-01 19:58:26 +0900

    feature: Success to build bare metal rust using panic_implementation.

--- BlogOS/blog_os/src/main.rs
+++ BlogOS/blog_os/src/main.rs
@@ -1,9 +1,14 @@
-#![feature(lang_items)]
+#![feature(panic_implementation)]
 #![no_std]  // Don't link the Rust standard library
 #![no_main]  // Disable all Rust-level entry points
 
-#[lang = "panic_fmt"]
-fn panic_fmt() -> ! { loop {} }
+use core::panic::PanicInfo;
+
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
 #[no_mangle]  // Don't mangle the name of the function
 pub extern "C" fn _start() -> ! {
