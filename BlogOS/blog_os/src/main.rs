5cdb1c9aef09950eb7fd53c7eb709133a31efd31
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-02 21:22:43 +0900

    feature: [Blog OS] VGA Buffer module displays ascii strings. #5

--- BlogOS/blog_os/src/main.rs
+++ BlogOS/blog_os/src/main.rs
@@ -2,6 +2,8 @@
 #![no_std]  // Don't link the Rust standard library
 #![no_main]  // Disable all Rust-level entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_implementation]
@@ -10,19 +12,11 @@ pub fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]  // Don't mangle the name of the function
 pub extern "C" fn _start() -> ! {
     // This function is the entry point, since the linker looks for a function
     // named `_start` by default.
-    let vga_buffer = 0xb8000 as *mut u8;
+    vga_buffer::print_something();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;  // color: light cyan
-        }
-    }
     loop {}
 }
