3d98be5a02a61209787a330fbd886063264705d2
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-06 20:50:09 +0900

    feature: BlogOS add formatting macros.

--- BlogOS/blog_os/src/vga_buffer.rs
+++ BlogOS/blog_os/src/vga_buffer.rs
@@ -1,4 +1,5 @@
 use volatile::Volatile;
+use core::fmt;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -87,7 +88,15 @@ impl Writer {
   fn new_line(&mut self) { /* TODO */ }
 }
 
+impl fmt::Write for Writer {
+  fn write_str(&mut self, s: &str) -> fmt::Result {
+    self.write_string(s);
+    Ok(())
+  }
+}
+
 pub fn print_something() {
+  use core::fmt::Write;
   let mut writer = Writer {
     column_position: 0,
     color_code: ColorCode::new(Color::Yellow, Color::Black),
@@ -95,6 +104,6 @@ pub fn print_something() {
   };
 
   writer.write_byte(b'H');
-  writer.write_string("ello ");
-  writer.write_string("Wörld!");
+  writer.write_str("ello ").unwrap();
+  write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
