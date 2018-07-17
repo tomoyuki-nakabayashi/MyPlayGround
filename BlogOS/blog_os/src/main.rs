c31e32db4a73a3130cffd286c5a0f40aa2bd3aee
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-17 13:01:02 +0900

    [BlogOS] Add exit_qemu().

--- BlogOS/blog_os/src/main.rs
+++ BlogOS/blog_os/src/main.rs
@@ -15,6 +15,8 @@ extern crate lazy_static;
 extern crate volatile;
 extern crate spin;
 extern crate uart_16550;
+extern crate x86_64;
+
 #[macro_use]
 mod serial;
 
@@ -39,5 +41,14 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");  // prints to vga buffer
     serial_println!("Hello Host{}", "!");
 
+    unsafe { exit_qemu(); }
+
     loop {}
+}
+
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(0);
 }
