6a2f7980e3bb94bad7d6730a08ce2e54c6a717d8
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-21 22:27:13 +0900

    [BlogOS 1st] Support Formatting Macros.

--- BlogOS/first_edition/src/lib.rs
+++ BlogOS/first_edition/src/lib.rs
@@ -1,21 +1,17 @@
 #![feature(panic_implementation)]
 #![no_std]
+#![feature(unique)]
+#![feature(const_fn)]
+#![feature(const_unique_new)]
+#![feature(ptr_internals)]
+
+mod vga_buffer;
+
+extern crate volatile;
 
 #[no_mangle]
 pub extern fn rust_main() {
-  // ATTENTION: we have a very small stack and no guard page
-
-  let hello = b"Hello World!";
-  let color_byte = 0x1f; // white foreground, blue background
-
-  let mut hello_colored = [color_byte; 24];
-  for (i, char_byte) in hello.into_iter().enumerate() {
-      hello_colored[i*2] = *char_byte;
-  }
-
-  // write `Hello World!` to the center of the VGA text buffer
-  let buffer_ptr = (0xb8000 + 1988) as *mut _;
-  unsafe { *buffer_ptr = hello_colored };
+    vga_buffer::print_something();
 
   loop{}
 }
