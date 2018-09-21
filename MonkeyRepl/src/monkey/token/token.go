46e2bd28195a0db2364d3ced97e922dcf7aab0d0
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-22 05:23:20 +0900

    monkey repl: Pass first lexer test.

---
+++ MonkeyRepl/src/monkey/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifier + literal
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// operator
+	ASSIGN = "="
+	PLUS   = "PLUS"
+
+	// delimiter
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keyword
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
