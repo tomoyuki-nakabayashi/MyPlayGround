e0c7cba32e8d8d23e90af0cda908f1fc2bd17b9d
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-10-04 21:18:06 +0900

    monkey repl: pp.201 Evaluate array.

--- MonkeyRepl/src/monkey/evaluator/evaluator.go
+++ MonkeyRepl/src/monkey/evaluator/evaluator.go
@@ -67,6 +67,12 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return evalInfixExpression(node.Operator, left, right)
 	case *ast.IfExpression:
 		return evalIfExpression(node, env)
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.StringLiteral:
