4386e1681b3ef4f6b27550f8e0f5997c578df104
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-10-07 20:08:46 +0900

    BlogOS 1st: Obtain available memory area from multiboot. Improve panic handling.

--- BlogOS/first_edition/src/lib.rs
+++ BlogOS/first_edition/src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(panic_implementation)]
+#![feature(panic_info_message)]
 #![no_std]
 #![feature(unique)]
 #![feature(const_fn)]
@@ -10,13 +11,21 @@ mod vga_buffer;
 
 extern crate volatile;
 extern crate spin;
+extern crate multiboot2;
 
 #[no_mangle]
-pub extern fn rust_main() {
+pub extern fn rust_main(multiboot_information_address: usize) {
     vga_buffer::clear_screen();
-    println!("Hello World{}", "!");
-    println!("{}", { println!("inner"); "outer" });
-
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag()
+        .expect("Memory map tag required");
+    
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}",
+            area.base_addr, area.length);
+    }
+    
     loop{}
 }
 
@@ -25,6 +34,19 @@ use core::panic::PanicInfo;
 #[cfg(not(test))] // only compile when the test flag is not set
 #[panic_implementation]
 #[no_mangle]
-pub fn panic(_info: &PanicInfo) -> ! {
+pub fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!("panic occured in file '{}' at line {}",
+            location.file(), location.line());
+    } else {
+        println!("panic occurred but can't get location information...");
+    }
+
+    if let Some(message) = info.message() {
+        println!("    {}", message);
+    } else {
+        println!("panic occurred but can't get any message...");
+    }
+
     loop {}
 }
