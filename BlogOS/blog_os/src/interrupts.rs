503d4c27b8847cd6cef90c248a2b39fb39ab1a5c
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-10-23 15:26:37 +0900

    BlogOS: Keyboard Input.

--- BlogOS/blog_os/src/interrupts.rs
+++ BlogOS/blog_os/src/interrupts.rs
@@ -5,6 +5,7 @@ use x86_64::structures::idt::{InterruptDescriptorTable, ExceptionStackFrame};
 use spin;
 
 pub const TIMER_INTERRUPT_ID: u8 = PIC_1_OFFSET;
+pub const KEYBOARD_INTERRUPT_ID: u8 = PIC_1_OFFSET + 1;
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -16,6 +17,8 @@ lazy_static! {
         }
         idt[usize::from(TIMER_INTERRUPT_ID)]
             .set_handler_fn(timer_interrupt_handler);
+        idt[usize::from(KEYBOARD_INTERRUPT_ID)]
+            .set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -42,6 +45,33 @@ extern "x86-interrupt" fn timer_interrupt_handler (
 {
     print!(".");
     unsafe { PICS.lock().notify_end_of_interrupt(TIMER_INTERRUPT_ID) }
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: &mut ExceptionStackFrame)
+{
+    use x86_64::instructions::port::Port;
+
+    let port = Port::new(0x60);
+    let scancode: u8 = unsafe { port.read() };
+
+    let key = match scancode {
+        0x02 => Some('1'),
+        0x03 => Some('2'),
+        0x04 => Some('3'),
+        0x05 => Some('4'),
+        0x06 => Some('5'),
+        0x07 => Some('6'),
+        0x08 => Some('7'),
+        0x09 => Some('8'),
+        0x0a => Some('9'),
+        0x0b => Some('9'),
+        _ => None,
+    };
+    if let Some(key) = key {
+        print!("{}", key);
+    }
+    unsafe { PICS.lock().notify_end_of_interrupt(KEYBOARD_INTERRUPT_ID) }
 }
 
 pub const PIC_1_OFFSET: u8 =  32;
