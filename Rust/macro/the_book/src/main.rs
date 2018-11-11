55584fce828765aae2b0e526dae4f916e88a86b6
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-11-11 20:48:17 +0900

    Rust: macro practice.

---
+++ Rust/macro/the_book/src/main.rs
@@ -0,0 +1,17 @@
+macro_rules! my_vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+fn main() {
+    let v = my_vec![1, 2, 3];
+
+    println!("{:?}", &v);
+}
