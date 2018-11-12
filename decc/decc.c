f623797eb39beb69f63508597e7625cd9cb0d5b4
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-11-12 12:30:05 +0900

    decc: Implementing tokenizer.

--- decc/decc.c
+++ decc/decc.c
@@ -1,5 +1,62 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// token identifiers
+enum {
+    TK_NUM = 256,  // Integer token
+    TK_EOF,        // End of input
+};
+
+typedef struct {
+    int ty;      // Token type
+    int val;     // The value if ty is TK_NUM
+    char *input; // Token strings (for error message)
+} Token;
+
+// Preserve the tokenizer result.
+// Assumes tokens up to 100.
+Token tokens[100];
+
+// Split token from strings pointed by p.
+void tokenize(char *p) {
+    int i = 0;
+    while (*p) {
+        // skip spaces
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            tokens[i].ty = *p;
+            tokens[i].input = p;
+            i++;
+            p++;
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            tokens[i].ty = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].val = strtol(p, &p, 10);
+            i++
+            continue;
+        }
+
+        fprintf(stderr, "Cannot tokenize: %s\n", p);
+        exit(1);
+    }
+
+    tokens[i].ty = TK_EOF;
+    tokens[i].input = p;
+}
+
+void error(int i) {
+    fprintf(stderr, "Unexpected token: %s\n", tokens[i].input);
+    exit(1);
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -7,16 +64,21 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char *p = argv[1];
+    tokenize(argv[1]);
 
+    // Output the upper half of assembly
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax,  %ld\n", strtol(p, &p, 10));
 
-    while (*p) {
-        if (*p == '+') {
-            p++;
+    if (tokens[0].ty != TK_NUM)
+        error(0);
+    printf("  mov rax,  %d\n", tokens[0].val);
+
+    int i = 1;
+    while (tokens[i].ty != TK_EOF) {
+        if (tokens[i].ty == '+') {
+            i++;
             printf("  add rax,  %ld\n", strtol(p, &p, 10));
             continue;
         }
