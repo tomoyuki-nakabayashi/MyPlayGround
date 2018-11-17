6a87256853fb98a6df24a5d737211e2c5e599e0c
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-11-17 14:48:55 +0900

    Rust: [Embedonomicon] Memory Layout.

--- Embedonomicon/app/src/main.rs
+++ Embedonomicon/app/src/main.rs
@@ -7,3 +7,16 @@ use core::panic::PanicInfo;
 fn panic(_panic: &PanicInfo<'_>) -> ! {
     loop {}
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn Reset() -> ! {
+    let _x = 42;
+
+    // can't return so we go into an infinite loop here.
+    loop {}
+}
+
+// The reset vector, a pointer into the reset handler
+#[link_section = ".vector_table.reset_vector"]
+#[no_mangle]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
